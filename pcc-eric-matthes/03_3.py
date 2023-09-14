a7c1461ce1f505c367c22aee93a0841842ddc199
hridyesh <114504644+vndlize@users.noreply.github.com>
2023-09-14 13:09:37 +0530

    Add files via upload

---
+++ pcc-eric-matthes/03_3.py
@@ -0,0 +1,3 @@
+vehicles = ['lambo','duke','ferrari']
+for vehicle in vehicles :
+    print (f'I would like to own a {vehicle.title()}.')
---
+++ pcc-eric-matthes/03_4.py
@@ -0,0 +1,3 @@
+gl = ['tom','jim','mark','brian','tim','cypher']
+for name in gl :
+    print (f'Hello {name.title()}, You are invited to Hridyesh\'s dinner party.')
