4a88e7320fb07f98ab42de1cb5ff0f60c897f8e3
hridyesh <hridyesh.das@gmail.com>
2023-10-17 14:21:07 +0530

    >>>

--- machine-learning/Neural Networks/03 convolutional layer.py
+++ machine-learning/Neural Networks/03 convolutional layer.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 
-# Define a sample input with dimensions (batch_size, height, width, channels)
-input_shape = (None, 28, 28, 3)  # None in batch_size means it can vary
+# Create a Sequential model and add the Conv2D layer to it
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='valid', activation='relu', input_shape=(28, 28, 3))
+])
 
-# Create a Conv2D layer with 32 filters, a 3x3 kernel, and 'valid' padding
-conv_layer = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='valid', activation='relu', input_shape=input_shape[1:])
-
-# Print layer summary
-conv_layer.summary()
+# Print the model summary
+model.summary()
